71ed1acbdec1d967f479d58ad7af2624d45f0a89
Amin Zaki mohamed <101603364+Aminzaki1972@users.noreply.github.com>
2026-09-22 19:02:00 +0300

    feat: support file uploads in Android WebView

--- app/src/main/java/xyz/web3market/android/MainActivity.java
+++ app/src/main/java/xyz/web3market/android/MainActivity.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.webkit.CookieManager;
 import android.webkit.DownloadListener;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -24,8 +26,11 @@ import androidx.webkit.WebViewFeature;
 public class MainActivity extends AppCompatActivity {
 
     private static final String HOME_URL = "https://web3market.xyz/";
+    private static final int FILE_CHOOSER_REQUEST = 1001;
+
     private WebView webView;
     private ProgressBar progressBar;
+    private ValueCallback<Uri[]> filePathCallback;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -73,6 +78,30 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(WebView view, int progress) {
                 progressBar.setVisibility(progress < 100 ? View.VISIBLE : View.GONE);
             }
+
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView,
+                    ValueCallback<Uri[]> callback,
+                    FileChooserParams params
+            ) {
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(null);
+                }
+
+                filePathCallback = callback;
+
+                Intent intent = params.createIntent();
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                try {
+                    startActivityForResult(intent, FILE_CHOOSER_REQUEST);
+                    return true;
+                } catch (ActivityNotFoundException e) {
+                    filePathCallback = null;
+                    return false;
+                }
+            }
         });
 
         webView.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
@@ -96,6 +125,32 @@ public class MainActivity extends AppCompatActivity {
         } else {
             webView.restoreState(savedInstanceState);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode != FILE_CHOOSER_REQUEST || filePathCallback == null) {
+            return;
+        }
+
+        Uri[] results = null;
+
+        if (resultCode == RESULT_OK) {
+            if (data != null && data.getClipData() != null) {
+                int count = data.getClipData().getItemCount();
+                results = new Uri[count];
+                for (int i = 0; i < count; i++) {
+                    results[i] = data.getClipData().getItemAt(i).getUri();
+                }
+            } else if (data != null && data.getData() != null) {
+                results = new Uri[]{data.getData()};
+            }
+        }
+
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
     }
 
     private boolean handleUrl(Uri uri) {
@@ -146,6 +201,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
+        if (filePathCallback != null) {
+            filePathCallback.onReceiveValue(null);
+            filePathCallback = null;
+        }
+
         if (webView != null) {
             webView.loadUrl("about:blank");
             webView.stopLoading();
